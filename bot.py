4f7c3e332e97337b993d5fefa3acfcf84fcf2b97
CblPOCHEK <sasadevatvoskij@gmail.com>
2024-05-27 14:26:07 +0300

    1

--- bot.py
+++ bot.py
@@ -1,48 +1,64 @@
 import requests
-from aiogram import Bot, Dispatcher, types, executor
+import base64
+import time
+from random import randint
+from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = '6866744247:AAH7pTTKqL9z5MS4JcaOmMkUauyQ94IW1U8'
-bot = Bot(token=API_TOKEN)
+bot = Bot(token='6650314598:AAEtbPwZYa6-E4UpTyQzsizvJs-pSqicf98')
 dp = Dispatcher(bot)
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message) :
-  await message.reply('Привет, я нейроконсультант который поможет подготовиться тебе к собеседованию. Напиши название компании должность которую ты претендуешь')
+  await message.reply('Привет, я твой шедеврум на минималках')
 
-async def get_response(message_text):
-  prompt = {
-    "modelUri": "gpt://b1gf5pfjsd0phsrq2ldh/yandexgpt-lite",
-    "completionOptions": {
-      "stream": False,
-      "temperature": 0,
-      "maxTokens": "2000"
-    },
-    "messages": [
-      {
-        "role": "system",
-        "text": "Ты — рекрутер в указанной компании. Имитируй собеседование на работу для указанной должности, задавая вопросы, как будто ты потенциальный работодатель. Твоя задача — определить технические навыки кандидата. Сгенерируй вопросы для интервью с потенциальным кандидатом"
-      },
-      {
-        "role": "user",
-        "text": message_text
+def generate_image(prompt_text):
+    promt = {
+          "modelUri": "art://b1g3f13cj7d6d3ss2md9/yandex-art/latest",
+          "generationOptions": {
+            "seed": randint(1, 20000000),
+          },
+          "messages": [
+            {
+              "weight": 1,
+              "text": prompt_text
+            }
+          ]
       }
-    ]
-  }
 
-  url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-  headers = {
-      "Content-Type": "application/json",
-      "Authorization": "Api-Key "
-  }
+    url = "https://llm.api.cloud.yandex.net/foundationModels/v1/imageGenerationAsync"
 
-  response = requests.post(url, headers=headers, json=prompt)
-  result = response.json()
-  return result['result']['alternatives'][0][ 'message']['text']
+    headers = {
+          "Content-Type": "application/json",
+          "Authorization": "Api-Key AQVNy07LcVvM2d6W_-sH-4jfymTfPqrkNUfEEw1R"
+      }
+
+    response = requests.post(url= url, headers=headers, json=promt)
+    result = response.json()
+    print(result)
+
+    operation_id = result['id']
+
+    operation_url = f"https://llm.api.cloud.yandex.net:443/operations/{operation_id}"
+
+    while True:
+        operation_response = requests. get(operation_url, headers=headers)
+        operation_result = operation_response.json()
+        if 'response' in operation_result:
+            image_base64 = operation_result['response']['image']
+            image_data = base64.b64decode(image_base64)
+            return image_data
+        else:
+            time.sleep(5)
 
 @dp.message_handler()
-async def analize_message (message:types.Message):
-  response_text = await get_response ((message.text))
-  await message.answer(response_text)
+async def handle_message(message: types.Message) :
+    user_text = message.text
+    await message.reply('Идет генерация изображения, подождите')
+    try:
+        image_data = generate_image(user_text)
+        await message.reply_photo(image_data)
+    except Exception as e:
+        await message.reply(f'Произошла ошибка {e}')
 
 if __name__ == '__main__':
   executor.start_polling(dp, skip_updates=True)
